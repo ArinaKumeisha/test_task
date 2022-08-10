3c27d09f9da9bebd7e01743922ce2a3c2a8a55c3
ArinaKumeisha <arina@elerna.online>
2022-08-10 18:36:27 +0300

    test

---
+++ 02-collapse/index.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelector('.collapsible');
+  
+  const handleToggle = (event) => {
+    const parent = event.target.closest(".collapsible");
+    const content = parent.querySelector('.collapsible__content');
+    const buttonHide = parent.querySelector('.collapsible__action--visible');
+    const buttonShow = parent.querySelector('.collapsible__action--hidden');
+    
+    if (buttonHide.style.display === 'none') {
+      buttonHide.style.display = 'block';
+      buttonShow.style.display = 'none';
+      content.animate([{transform: 'scale(1, 1)', maxHeight: '1000px'}], {
+        duration: 300,
+        fill: 'forwards'
+      });
+    } else {
+      buttonHide.style.display = 'none';
+      buttonShow.style.display = 'block';
+      content.animate([{transform: 'scale(1, 0)', maxHeight: 0}], {
+        duration: 300,
+        fill: 'forwards'
+      });
+    }
+  }
+  
+  for (const collapsible of items) {
+    const buttonToggle = collapsible.querySelector('.collapsible__button');
+    const buttonHide = buttonToggle.querySelector('.collapsible__action--visible');
+    const content = collapsible.querySelector('.collapsible__content');
+    buttonToggle.addEventListener('click', handleToggle);
+    buttonHide.style.display = 'none';
+    
+    content.animate([{transform: 'scale(1, 0)', maxHeight: 0}], {
+      duration: 0,
+      fill: 'forwards'
+    });
+  }
+});
